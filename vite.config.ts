b727569f30d056aea2b5de8e9ea504630f68d0c3
riki <aguilcasariki@gmail.com>
2024-11-06 19:10:03 -0500

    Add app env

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(() => {
+  return {
+    plugins: [react()],
+    define: {
+      __APP_ENV__: process.env.VITE_VERCEL_ENV,
+    },
+  };
+});
