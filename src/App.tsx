6fa99d4d7b966652201fa252a23f3f1321cf3ab4
riki <aguilcasariki@gmail.com>
2024-11-07 00:20:11 -0500

    Test add vercel to env

--- src/App.tsx
+++ src/App.tsx
@@ -27,12 +27,12 @@ console.log(import.meta.env);
 
 // Initialize Firebase app
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: import.meta.env.VITE_VERCEL_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_VERCEL_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_VERCEL_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_VERCEL_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_VERCEL_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_VERCEL_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
